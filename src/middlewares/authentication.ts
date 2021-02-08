9fe13dcce9f88d11c72773ef7a51b64369fccb27
Ganjar <zarszzntunx@rocketmail.com>
2021-02-08 08:28:03 +0700

    fix: fix linter error

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -4,10 +4,10 @@ import { validateJWT } from '../utils/auth.util';
 
 export async function verifyToken(req: Request, res: Response, next: NextFunction): Promise<void | Response> {
     const bearer = req.header('token')!;
-    if (!bearer) return res.status(400).send({status: 'Error', message: 'No token provided !!'});
+    if (!bearer) return res.status(400).send({ status: 'Error', message: 'No token provided !!' });
     const token = bearer.split(' ')[1];
 
     const verified_token = await validateJWT(token as string);
-    if (!verified_token['id']) res.status(400).send({status: 'Error', message: 'Invalid token provided !!'});
+    if (!verified_token['id']) res.status(400).send({ status: 'Error', message: 'Invalid token provided !!' });
     next();
 }
